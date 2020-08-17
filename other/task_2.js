aadb6860eb6431f0f79ed6a394eefb046af7c429
TimNoV3 <tilichev@mail.ru>
2020-08-17 16:38:24 +0300

    task_2

---
+++ other/task_2.js
@@ -0,0 +1,13 @@
+max_code = '26';
+
+const waysToDecode = message => {
+  if (message[0] === '0') return 0;
+  if (message.length === 0) return 1
+  else {
+    if (message.length === 1 || message.slice(0,2) > max_code) return waysToDecode(message.slice(1))
+    else return waysToDecode(message.slice(2)) + waysToDecode(message.slice(1));
+  }
+}
+
+
+console.log(waysToDecode('11111111111'));
